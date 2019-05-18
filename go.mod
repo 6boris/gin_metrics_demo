474ecba55e5d98c55918adfee43a3682b7de428f
Kyle Liu <kylesliu@outlook.com>
2019-05-18 18:17:45 +0800

    更新url

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kylesliu/gin_exporter_demo
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/kylesliu/gin_metrics v0.0.4 // indirect
+)
--- main.go
+++ main.go
@@ -3,46 +3,49 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/kylesliu/gin_exporter"
+	"github.com/kylesliu/gin_metrics"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 func main() {
-	app := gin.Default()
+	app := gin.New()
 	gin.SetMode(gin.DebugMode)
 
-	gin.IsDebugging()
-
-	app.GET("demo1", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"code": 200,
-			"msg":  "demo1",
-		})
+	app.GET("rand_str", func(c *gin.Context) {
+		c.JSON(200, getRandomString(32))
 	})
-
-	app.GET("demo2", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"code": 200,
-			"msg":  "demo1",
-		})
+	app.GET("rand_int", func(c *gin.Context) {
+		c.JSON(200, rand.Intn(100000000000))
 	})
-
-	app.GET("demo3", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"code": 200,
-			"msg":  "demo1",
-		})
+	app.GET("time_now", func(c *gin.Context) {
+		c.JSON(200, time.Now())
 	})
+	app.GET("rand_sleep", func(c *gin.Context) {
+		time.Sleep(time.Duration(time.Duration(rand.Intn(1000)) * time.Millisecond))
+		c.JSON(200, time.Now())
+	})
+	for i := 0; i < 100; i++ {
+		app.GET(fmt.Sprint("rand_route", strconv.Itoa(i)), func(c *gin.Context) {
+			c.JSON(200, time.Now())
+		})
+	}
 
-	app = gin_exporter.Default(app)
+	gin_metrics.Default(app)
 
 	if err := app.Run("127.0.0.1:9000"); err != nil {
 		panic(err.Error())
 	}
 }
 
-func metricsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		fmt.Println(c.ClientIP())
-		c.Next()
+func getRandomString(str_len int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < str_len; i++ {
+		result = append(result, bytes[r.Intn(str_len)])
 	}
+	return string(result)
 }
